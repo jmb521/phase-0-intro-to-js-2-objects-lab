14172bebcb2471669a7949fb6128ac307f5c428c
Jennifer Pazos <jmp521@gmail.com>
2022-05-02 17:22:10 -0500

    commit tree f067d4b2bb9e2133482e7992dded39b77a0598e8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jennifer", 
+    streetAddress: "123 Main St."
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, val) {
+    return {...employee, [key]:  val}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, val) {
+    employee[key] = val
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
